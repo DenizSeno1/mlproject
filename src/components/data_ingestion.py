1eb503c2fd19e14515974fde5187a159e2be48a4
DenizSeno1 <ds.denizsenol@gmail.com>
2026-07-02 19:18:08 +0300

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,8 @@
 import os
-import random
 import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass
 
